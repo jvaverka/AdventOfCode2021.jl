e82230d42c35e4a0b4dc79d6c137690cc5d69ec4
Jake Vaverka <jvaverka@protonmail.com>
2021-12-01 09:58:52 -0500

    [Day 1] Complete

---
+++ src/day01.jl
@@ -0,0 +1,38 @@
+module Day01
+
+using AdventOfCode2021
+
+function day01(input::String = readInput(joinpath(@__DIR__, "..", "data", "day01.txt")))
+    values = parse.(Int, split(input))
+    return [part1(values), part2(values)]
+end
+
+const NOT_AVAILABLE = "(N/A - no previous measurement)"
+const INCREASED = "(increased)"
+const DECREASED = "(decreased)"
+
+function part1(values::Array{Int,1})
+    change = String[]
+    push!(change, NOT_AVAILABLE)
+    for (i, v) in enumerate(Iterators.rest(values, 2))
+        v > values[i] && push!(change, INCREASED)
+        v < values[i] && push!(change, DECREASED)
+    end
+    return count(==(INCREASED), change)
+end
+
+function part2(values::Array{Int,1})
+    windows = []
+    for i in 1:length(values)-2
+        push!(windows, sum(values[i:i+2]))
+    end
+    change = String[]
+    push!(change, NOT_AVAILABLE)
+    for (i, w) in enumerate(Iterators.rest(windows, 2))
+        w > windows[i] && push!(change, INCREASED)
+        w < windows[i] && push!(change, DECREASED)
+    end
+    return count(==(INCREASED), change)
+end
+
+end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,2 +1,9 @@
 using AdventOfCode2021
 using Test
+
+@testset "Day 1" begin
+    sample = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
+    @test AdventOfCode2021.Day01.part1(sample) == 7
+    @test AdventOfCode2021.Day01.part2(sample) == 5
+    @test AdventOfCode2021.Day01.day01() == [1709, 1761]
+end
